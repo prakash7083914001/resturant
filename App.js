0e8ca9734c47858fb62dec80c323856430798253
prakash7083914001 <91177688+prakash7083914001@users.noreply.github.com>
2021-09-22 12:48:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+//import React, { useState } from "react";
+//import UseEffect from "./component/Hooks/useEffect";
+ import Resturant from "./component/Basics/Resturant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+//import Login from "./component/Login/Login";
+const App = () => {
+  return <Resturant />;
+};
+/*
+function App() {
+  return (
+    <div >
+      <Router>
+        <Switch>
+             <Route exact strict path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+*/
+export default App;
